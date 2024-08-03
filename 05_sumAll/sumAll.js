71cefcfe18ae507d83245095e34b4b0ccc2aa043
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-08-03 17:29:37 +1000

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    let startNum = Math.min(num1, num2);
+    let endNum = Math.max(num1, num2);
+
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
